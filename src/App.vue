83f6779410569e355e288963635a1904001566b5
Estuardo <eramos.9588@gmail.com>
2021-04-16 21:56:23 -0600

    inital commit: replacing HelloWorld component
    Using TodoListView component instead, an commenting  previus component
    import and vue logo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
+  <TodoListView />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+//import HelloWorld from "./components/HelloWorld.vue";
+import TodoListView from "./components/TodoListView.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld
-  }
+    //  HelloWorld,
+    TodoListView,
+  },
 });
 </script>
 
