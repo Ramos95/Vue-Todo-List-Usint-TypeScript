2a410cc3ac7ba27038770bf18809f5dcf149cff0
Estuardo <eramos.9588@gmail.com>
2021-04-16 21:57:49 -0600

    inita commit: radiobutton component
    radio button component to reutilize multpiple times and
    update changes to parent view

---
+++ src/components/RadioButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-for="(option, index) in optionList" :key="index">
+    <input
+      :id="option.text"
+      :key="index"
+      class="inputRadio"
+      type="radio"
+      :value="option.status"
+      v-model.number="selectedOption"
+      @change="handleOptionChange"
+    />
+    <label :for="option.text">{{ option.text }}</label>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import { SelectOptions } from "../types";
+
+export default defineComponent({
+  props: {
+    optionList: {
+      type: Object as PropType<SelectOptions[]>,
+    },
+  },
+  emits: ["updateSelection"],
+
+  data() {
+    return {
+      selectedOption: 0,
+    };
+  },
+  methods: {
+    handleOptionChange: function() {
+      this.$emit("updateSelection", this.selectedOption);
+    },
+  },
+});
+</script>
